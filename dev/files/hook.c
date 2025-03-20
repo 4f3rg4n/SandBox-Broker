31996dc6470a0d77a7b14342c318987fd6c2d85b
AI-fergan <ifrganvrcturh@gmail.com>
2025-03-20 11:46:07 +0200

    Update files hook lib & add comments

--- dev/files/hook.c
+++ dev/files/hook.c
@@ -7,19 +7,33 @@ fd_obj* fd_list = NULL;
 
 unsigned long int hook_id = 0;
 
-char* files_hook_hanlder(const char* path, const char* func, int flags) {
+/*
+This function handle files functions hooks.
+Input:
+path - file path
+func - function name
+flags - function flags
+buf - buffer that need to be pass to the real function
+Output: function new path.
+*/
+char* files_hook_hanlder(const char* path, const char* func, int flags, const void* buf) {
     char op;
     char* new_path = (char*)calloc(sizeof(char), PATH_MAX);
 
+    /* Hook data */
     printf("- [FILE HOOK {ID = %ld}] -\n", hook_id++);
     printf("++ hook data ++\n");
+    printf("buf: %s\n", buf);
     printf("path: %s\n", path);
     printf("func: %s\n", func);
     printf("flags: %d\n", flags);
 
+    /* Handle function path modification */
     printf("modify path? (y/n): ");
     op = getchar();
     getchar();
+
+    //new path
     if (op == 'y') {
         printf("path: ");
         fgets(new_path, PATH_MAX, stdin);
@@ -31,23 +45,37 @@ char* files_hook_hanlder(const char* path, const char* func, int flags) {
         printf("continue function process? (y/n): ");
         op = getchar();
         getchar();
-        if (op == 'y')
+        if (op == 'y') //continue with old path
             return new_path;
-        return NULL;
+        return NULL; //terminate function
     }
 }
 
+/* 
+This function return file path by its id using global files linked list.
+Input:
+fd - file fd number
+Output: file path.
+*/
 char* get_path_from_fd(int fd) {
     fd_obj* obj = fd_list;
-    while(obj) {
+
+    while(obj) { //loop until get the file path
         if(obj->fd == fd)
             return obj->path;
         obj = obj->next;
     }
 
-    return NULL; //error
+    return NULL; //error: path not found!
 }
 
+/*
+This function add fd number with file path to the global fd list.
+Input:
+fd- file fd number
+path - file path
+Output: NULL.
+*/
 void add_fd_object(int fd, char* path) {
     fd_obj* obj = (fd_obj*)malloc(sizeof(fd_obj));
     obj->fd = fd;
@@ -56,45 +84,75 @@ void add_fd_object(int fd, char* path) {
     fd_list = obj;
 }
 
+/*
+This function is the hook function of the open syscall func.
+Input:
+path - file path
+flags - open flags
+Output: file fd.
+*/
 ssize_t open_hook(const char *path, int flags, ...) {
     int fd;
-    mode_t mode = 0;
 
-    // Check if the real open function is already saved
+    //check if the real open function is already saved
     if (!real_open)
-        real_open = dlsym(RTLD_NEXT, "open"); // Get open symbol
+        real_open = dlsym(RTLD_NEXT, "open"); //get open runtime addr
 
-    // Handle optional third argument (mode) using va_list
-    if (flags & O_CREAT) {
-        va_list args;
-        va_start(args, flags);
-        mode = va_arg(args, mode_t); // Get mode value if O_CREAT is used
-        va_end(args);
-    }
-
-    char* new_path = files_hook_hanlder(path, "open", flags);
-    fd = real_open(new_path, flags, mode);  // Pass mode if present
-    add_fd_object(fd, new_path);
+    char* new_path = files_hook_hanlder(path, "open", flags, 0);
+    fd = real_open(new_path, flags); //run real open func
+    add_fd_object(fd, new_path); //add fd & path to global fd's list
 
     return fd;
 }
 
+/*
+This function is the hook function of the read syscall func.
+Input:
+fd - file fd number
+buf - target buffer pointer to save data
+count - number of bytes to read
+Output: number of readed bytes.
+*/
 ssize_t read_hook(int fd, void *buf, size_t count) {
     ssize_t res;
 
-    // Check if the real read function is already saved
+    //check if the real read function is already saved
     if (!real_read)
-        real_read = dlsym(RTLD_NEXT, "read"); // Get read symbol
+        real_read = dlsym(RTLD_NEXT, "read"); //get read runtime addr
 
     char* path = get_path_from_fd(fd);
-    char* new_path = files_hook_hanlder(path, "read", O_RDONLY);
-    free(path);
+    char* new_path = files_hook_hanlder(path, "read", O_RDONLY, buf);
 
-    res = real_read(fd, buf, count);  // Call the real read function
+    res = real_read(fd, buf, count);  //call the real read function
     free(new_path);
     return res;
 }
 
-// We no longer define open() directly, we use alias to hook it
+/*
+This function is the hook function of the write syscall func.
+Input:
+fd - file fd number
+buf - target buffer pointer to write data from
+count - number of bytes to write
+Output: number of written bytes.
+*/
+ssize_t write_hook(int fd, const void *buf, size_t count) {
+    ssize_t res;
+
+    //check if the real write function is already saved
+    if (!real_read)
+        real_read = dlsym(RTLD_NEXT, "write"); //get runtime write addr
+
+    char* path = get_path_from_fd(fd);
+    char* new_path = files_hook_hanlder(path, "write", O_RDONLY, buf);
+
+    res = real_read(fd, buf, count);  //call the real write function
+    free(new_path);
+    return res;
+}
+
+/* Let the real open & read & write function become weak 
+and the hook functions strong so they will replace them */
 __attribute__((weak, alias("open_hook"))) int open(const char *path, int flags, ...);
 __attribute__((weak, alias("read_hook"))) ssize_t read(int fd, void *buf, size_t count);
+__attribute__((weak, alias("write_hook"))) ssize_t write(int fd, const void *buf, size_t count);
--- dev/files/hook.h
+++ dev/files/hook.h
@@ -7,17 +7,19 @@
 
 #define PATH_MAX 4096
 
+/* Used for global fd's list */
 typedef struct {
     int fd;
     char* path;
     struct fd_obj* next;
 } fd_obj;
 
-char* files_hook_hanlder(const char* path, const char* func, int flags);
+char* files_hook_hanlder(const char* path, const char* func, int flags, const void* buf);
 char* get_path_from_fd(int fd);
 void add_fd_object(int fd, char* path);
 ssize_t open_hook(const char *path, int flags, ...);
-
+ssize_t read_hook(int fd, void *buf, size_t count);
+ssize_t write_hook(int fd, const void *buf, size_t count);
 
 extern unsigned long int hook_id;
 extern fd_obj* fd_list;
